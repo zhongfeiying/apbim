1f497bfbb9b361a4648e61ca0016228ff77d190a
SJY2016 <1433653386@qq.com>
2017-05-18 17:06:58 +0800

    add some key !

--- apbim/app/LoginPro/language_package.lua
+++ apbim/app/LoginPro/language_package.lua
@@ -11,5 +11,40 @@ return {
 		English = 'Change Password';
 		Chinese = '修改密码';
 	};
-
+	['username'] = {
+		English = 'User Name : ';
+		Chinese = '用户名 ：';
+	};
+	['password'] = {
+		English = 'Password : ';
+		Chinese = '密码 ：';
+	};
+	['remember_password'] = {
+		English = 'Remember Password';
+		Chinese = '记住密码';
+	};
+	['auto_log'] = {
+		English = 'Auto Login';
+		Chinese = '自动登陆';
+	};
+	['register'] = {
+		English = 'Registering';
+		Chinese = '注册';
+	};
+	['retrieval'] = {
+		English = 'Password Retrieval';
+		Chinese = '找回密码';
+	};
+	['login_ok'] = {
+		English = 'Ok';
+		Chinese = '确认';
+	};
+	['cancel'] = {
+		English = 'Cancel';
+		Chinese = '取消';
+	};
+	['dlg_login'] = {
+		English = 'Login';
+		Chinese = '登陆';
+	};
 }
