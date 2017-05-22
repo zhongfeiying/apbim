5bc91a72fbd680d9470e5b037bec895df112c05d
SJY2016 <1433653386@qq.com>
2017-05-22 13:57:48 +0800

    change var name !

--- apbim/app/ProjectMgr/interface/workspace.lua
+++ apbim/app/ProjectMgr/interface/workspace.lua
@@ -10,18 +10,17 @@ _ENV = M
 local iup_ = require 'iuplua'
 require 'iupluacontrols'
 
-local workspace_ = require 'sys.workspace'
-local tree_ = require 'app.projectmgr.workspace.tree_control'
-local tree_project_list_ =  require 'app.projectmgr.workspace.tree_workspace'
-local project_list_;
+local sys_workspace_ = require 'sys.workspace'
+local tree_control_ = require 'app.projectmgr.workspace.tree_control'
+local workspace_tree_ =  require 'app.projectmgr.workspace.tree_workspace'
+local workspace_;
 
 
 local function init_control_data()
-	
 	return {
 		hwnd = iup_.frame{
 			iup_.vbox{
-				tree_.get_control();
+				tree_control_.get_control();
 				margin = '0x0';
 				alignment = 'ALEFT';
 			};
@@ -31,16 +30,16 @@ local function init_control_data()
 end
 
 local function load_project_list()
-	tree_.set(tree_project_list_)
-	tree_.init()
-	project_list_ = init_control_data()
-	workspace_.add(project_list_)
+	tree_control_.set(workspace_tree_)
+	tree_control_.init()
+	workspace_ = init_control_data()
+	sys_workspace_.add(workspace_)
 end 
 
 local function unload_project_list()
-	if project_list_ then 
-		workspace_.delete(project_list_.hwnd)
-		project_list_ = nil
+	if workspace_ then 
+		sys_workspace_.delete(workspace_.hwnd)
+		workspace_ = nil
 	end
 	
 end
