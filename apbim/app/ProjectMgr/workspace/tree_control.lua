4333e15aefa478c485b29ba32fd6c363c710c771
SJY2016 <1433653386@qq.com>
2017-05-22 13:12:03 +0800

    change it!

--- apbim/app/ProjectMgr/workspace/tree_control.lua
+++ apbim/app/ProjectMgr/workspace/tree_control.lua
@@ -36,3 +36,47 @@ function get_current_id()
 		return cmd_.get_current_id()
 	end
 end
+
+function get_current_id()
+	if type(cmd_.get_current_id) == 'function' then 
+		return cmd_.get_current_id()
+	end
+end
+
+function add_branch(name,id)
+	if type(cmd_.add_branch) == 'function' then 
+		return cmd_.add_branch(name,id)
+	end
+end
+
+function add_leaf(name,id)
+	if type(cmd_.add_leaf) == 'function' then 
+		return cmd_.add_leaf(name,id)
+	end
+end
+
+function insert_branch(name,id)
+	if type(cmd_.insert_branch) == 'function' then 
+		return cmd_.insert_branch(name,id)
+	end
+end
+
+function insert_leaf(name,id)
+	if type(cmd_.insert_leaf) == 'function' then 
+		return cmd_.insert_leaf(name,id)
+	end
+end
+
+function delete(id,status)
+	if type(cmd_.delete) == 'function' then 
+		return cmd_.delete(id,status)
+	end
+end
+
+function change(arg)
+	if type(cmd_.change) == 'function' then 
+		return cmd_.change(arg)
+	end
+end
+
+
