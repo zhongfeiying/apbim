153cb3c53f24c25960305568f2405847a5be8eca
SJY2016 <1433653386@qq.com>
2017-05-22 13:16:54 +0800

    rename templatefiles -> template

---
+++ apbim/app/ProjectMgr/template/User.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		name =get_username();
+		image = 'app/ProjectMgr/res/user.bmp';
+		{
+			{
+				name = '私人文件夹';
+				image = 'app/ProjectMgr/res/user.bmp';
+			};
+			{
+				name = '工程';
+				image = 'app/ProjectMgr/res/user.bmp';
+			};
+			{
+				name = '好友';
+				image = 'app/ProjectMgr/res/user.bmp';
+			};
+			{
+				name = '回收站';
+				image = 'app/ProjectMgr/res/user.bmp';
+			};
+		};
+	};
+}
