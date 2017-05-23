e78c3f597c2f34b34148b1890bd0b509f3c5cd53
zgb-pc <122757440@qq.com>
2017-05-23 09:38:20 +0800

    add sogi

---
+++ apbim/configure/plugin_browser.lua
@@ -0,0 +1,27 @@
+function open_plugin_db()
+end
+
+function get_selct_plugins()
+end
+
+function run()
+	--加载所有的选中pulgins
+end
+
+function install()
+end
+
+function uninstall()
+end
+
+function stop()
+end
+
+function refresh()
+end
+
+
+function update()
+end
+
+
