009336ef0712ce7bd0cb971fca5cad0a8fb30997
SJY2016 <1433653386@qq.com>
2017-05-23 09:16:19 +0800

    test iupTree new function !

--- apbim/sys/workspace/tree/test.lua
+++ apbim/sys/workspace/tree/test.lua
@@ -2,11 +2,27 @@ require 'iup_dev'
 local iuptree = require 'iupTree'
 require 'iuplua'
 tree = iuptree.Class:new()
-tree:init_path_data('E:\\Sync\\workingGit\\apbim\\apbim\\sys\\api')
+local function rule(str)
+	return {
+		icon = 'd:/user_open.bmp';
+		tip = 'hello';
+	}
+end
+tree:init_path_data('E:\\Sync\\workingGit\\apbim\\apbim\\sys\\api',rule)
 tree:set_rastersize('400x400')
+
+local function f(id)
+	print(tree:get_selected_path())
+end
+
+tree:set_lbtn(f)
+
+
 local dlg = iup.dialog{
 	tree:get_tree();
 }
+
+
 
 dlg:popup()
 
