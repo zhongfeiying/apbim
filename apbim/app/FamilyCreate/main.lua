8ef2ffa0f065d5ad30aa7698b2873275456d03db
SJY2016 <1433653386@qq.com>
2017-05-24 13:03:29 +0800

    ready do familycreate app

---
+++ apbim/app/FamilyCreate/main.lua
@@ -0,0 +1,21 @@
+
+local require = require
+ 
+local M = {}
+local modname = ...
+_G[modname] = M
+package.loaded[modname] = M
+_ENV = M
+
+ 
+function on_load()
+end
+
+function on_init()
+end
+
+function on_esc()
+
+
+end
+
