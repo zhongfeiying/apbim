c06ed819835ca5537c8c522fa25050c3ac6954b0
SJY2016 <1433653386@qq.com>
2017-05-25 21:00:47 +0800

    add dlg.lua in familycreate app

---
+++ apbim/app/FamilyCreate/dlg.lua
@@ -0,0 +1,15 @@
+
+local require =require
+local M = {}
+local modname = ...
+_G[modname] = M
+package.loaded[modname] =M
+_ENV = M
+
+
+local iup = require 'iuplua'
+local dlg_;
+
+function pop()
+	
+end
