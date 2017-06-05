5dbfa38f08d4ac7862dc893bf542fbe61357135c
SJY2016 <1433653386@qq.com>
2017-06-05 13:24:59 +0800

    change language

--- apbim/sys/language.lua
+++ apbim/sys/language.lua
@@ -1,34 +1,67 @@
+local package_loaded_ = package.loaded
+local require = require
+local io_open_ =  io.open
+local print = print
+local type = type
 
 local M = {}
 local modname = ...
 _G[modname] = M
-package.loaded[modname] = M
+package_loaded_[modname] = M
 _ENV = M
-
-local file = 'cfg.language'
+local code_ = require 'sys.api.code'
+local language_file_ = 'cfg/language.lua'
+local language_list_file_ = 'cfg/languageList.lua'
+local require_language_list_file_ = 'cfg.languageList'
+local require_language_file_ = 'cfg.language'
 
 local language_list_ = {
 	['English'] = 'English';
 	['中文简体'] = 'Chinese';
 }
-local language_ = '中文简体'
+
+local function save(file,t)
+	code_.save{file=file;src=t}
+end
+
+local function get_data(file,require_file)
+	local file =io_open_(file,'r')
+	if file then 
+		file:close()
+		package_loaded_[require_file] = nil
+		return   require (require_file) 
+	end
+end
+
+local function get_language()
+	local file,require_file = language_file_,require_language_file_;
+	local t =  get_data(file,require_file)
+	if type(t) == 'table' and t.language and language_list_[t.language] then 
+		return  t.language
+	else 
+		save(file,{language = 'English'})
+		return  'English'
+	end 
+end
+
+local function get_language_list()
+	local file,require_file = language_list_file_,require_language_list_file_;
+	return  get_data(file,require_file)
+end
 
 function set(language)
-	language_ = language and  language_list_[language] or language_
+	save(language_file_,{language = language})
 end
 
 function get()
-	return language_list_[language_]
+	local language = get_language() 
+	return language_list_[language]
 end
 
 function get_language_list()
 	return language_list_
 end
 
-
 function init()
-	local t = require  (file)
-	set(t and t.language)
+	-- get_language_list()
 end
-
-
